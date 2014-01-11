cd7a197154b348390b8c156b8b1aad0c12e12a9f
wangbinxiang <flywindspeed@gmail.com>
2014-01-12 02:35:28 +0800

    mysql conf

--- models/init.go
+++ models/init.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
@@ -10,13 +11,13 @@ var Orm orm.Ormer
 
 func MysqlRegisterDB() {
 
-	// mysqlUser := beego.AppConfig.String("mysqluser")
-	// mysqlPass := beego.AppConfig.String("mysqlpass")
-	// mysqlUrl := beego.AppConfig.String("mysqlurl")
-	// mysqlDB := beego.AppConfig.String("mysqldb")
-	// mysqlCharset := beego.AppConfig.String("mysqlcharset")
+	mysqlUser := beego.AppConfig.String("mysqluser")
+	mysqlPass := beego.AppConfig.String("mysqlpass")
+	mysqlHost := beego.AppConfig.String("mysqlhost")
+	mysqlPort := beego.AppConfig.String("mysqlport")
+	mysqlDB := beego.AppConfig.String("mysqldb")
 
-	orm.RegisterDataBase("default", "mysql", "root:@/codesave?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", mysqlUser, mysqlPass, mysqlHost, mysqlPort, mysqlDB))
 	Orm = orm.NewOrm()
 }
 
