62503daae23f1248d089f9ba7936cc9dd8c7b376
wangbinxiang <flywindspeed@gmail.com>
2014-01-28 17:58:42 +0800

    123

--- caches/memcache/init.go
+++ caches/memcache/init.go
@@ -10,7 +10,8 @@ var Memcache cache.Cache
 func init() {
 	var err error
 	if Memcache == nil {
-		Memcache, err = cache.NewCache("memcache", `{"conn":"127.0.0.1:11211"}`)
+		// Memcache, err = cache.NewCache("memcache", `{"conn":"127.0.0.1:11211"}`)
+		Memcache, err = cache.NewCache("memory", `{"interval":60}`)
 		if err != nil {
 			log.Println(err)
 		}
