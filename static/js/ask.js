6df25e1f24cb6a596c14c65442005b9e6a19295a
wangbinxiang <flywindspeed@gmail.com>
2014-02-09 01:04:44 +0800

    123

--- static/js/ask.js
+++ static/js/ask.js
@@ -1,7 +1,4 @@
 function contentKeyUp() {
 	$('#previewEditor').text($('#editor').val());
-	$('#previewEditor').removeClass($('#previewEditor').attr('class'));
-	$('pre code').each(function(i, e) {
-		hljs.highlightBlock(e)
-	});
+	prettyPrint();
 }
